752e1a1f5488a300dd3da8790ae32e63610c1ee9
Zachary Pleasant <zpleasant10@gmail.com>
2017-12-03 13:56:42 -0600

    changed js to show random country text in DOM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,12 @@
 var answers = ["Argentina", "Bolivia", "Brazil", "Chile", "Colombia", "Ecuador", "Guyana", "Paraguay", "Peru", "Suriname", "Uruguay", "Venezuela"];
 
-var randomIndex = Math.floor(Math.random()*answers.length);
-var randomAnswer = answers[randomIndex];
+var randomIndex = Math.floor(Math.random() * answers.length);
+var randomChoice = answers[randomIndex];
 
-console.log(randomAnswer);
+console.log(randomChoice);
+
+for (i = 0; i < randomChoice.length; i++) { 
+    console.log(randomChoice[i]);
+}
+
+document.getElementById("gameChoice").innerHTML = randomChoice;
