22ebb1a27a76c28dbace764c4dc6287392b38678
Zachary Pleasant <zpleasant10@gmail.com>
2017-12-03 15:26:49 -0600

    added split method to js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,18 @@
+// create an array with list of answers
 var answers = ["Argentina", "Bolivia", "Brazil", "Chile", "Colombia", "Ecuador", "Guyana", "Paraguay", "Peru", "Suriname", "Uruguay", "Venezuela"];
 
+// randomly select one of the words from the array
 var randomIndex = Math.floor(Math.random() * answers.length);
 var randomChoice = answers[randomIndex];
 
 console.log(randomChoice);
 
-for (i = 0; i < randomChoice.length; i++) { 
-    console.log(randomChoice[i]);
-}
+// use split method to separate randomChoice to letters
+var randomChoiceSplit = randomChoice.split("");
 
+console.log(randomChoiceSplit);
+
+// convert the randomly selected word to -- form
+
+// update DOM with the word
 document.getElementById("gameChoice").innerHTML = randomChoice;
