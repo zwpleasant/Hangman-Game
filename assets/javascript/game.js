cf14506ac69bd832bd78f6b567c806c6fbba94e7
Zachary Pleasant <zpleasant10@gmail.com>
2017-12-03 12:29:02 -0600

    added country list variable and select random country variable to js

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+var answers = ["Argentina", "Bolivia", "Brazil", "Chile", "Colombia", "Ecuador", "Guyana", "Paraguay", "Peru", "Suriname", "Uruguay", "Venezuela"];
+
+var randomIndex = Math.floor(Math.random()*answers.length);
+var randomAnswer = answers[randomIndex];
+
+console.log(randomAnswer);
