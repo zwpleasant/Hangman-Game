397e7329a9e90391fdc834c961a54da2b87a2dd7
Zachary Pleasant <zpleasant10@gmail.com>
2017-12-03 22:54:50 -0600

    added press any key to start function and some css edits

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,28 @@
-// create an array with list of answers
-var answers = ["Argentina", "Bolivia", "Brazil", "Chile", "Colombia", "Ecuador", "Guyana", "Paraguay", "Peru", "Suriname", "Uruguay", "Venezuela"];
+// function that starts when user types a key
+document.onkeyup = function(event) {
 
-// randomly select one of the words from the array
-var randomIndex = Math.floor(Math.random() * answers.length);
-var randomChoice = answers[randomIndex];
+	// create an array with list of answers
+	var answers = ["Argentina", "Bolivia", "Brazil", "Chile", "Colombia", "Ecuador", "Guyana", "Paraguay", "Peru", "Suriname", "Uruguay", "Venezuela"];
 
-console.log(randomChoice);
+	// randomly select one of the words from the array
+	var randomIndex = Math.floor(Math.random() * answers.length);
+	var randomChoice = answers[randomIndex];
 
-// use split method to separate randomChoice to letters
-var randomChoiceSplit = randomChoice.split("");
+	console.log(randomChoice);
 
-console.log(randomChoiceSplit);
+	// create an array with hidden randomChoice
+	var hiddenChoice = [];
+	for (var i = 0; i < randomChoice.length; i++) {
+		hiddenChoice[i] = "-";
+	}
 
-// convert the randomly selected word to -- form
+	// use the split method on randomChoice to make it an array of letters
+	var randomChoiceSplit = randomChoice.split("")
 
-// update DOM with the word
-document.getElementById("gameChoice").innerHTML = randomChoice;
+	var remainingLetters = hiddenChoice.length;
+
+	console.log(randomChoiceSplit);
+
+	// update DOM
+	document.getElementById("gameChoice").innerHTML = hiddenChoice.join(" ");
+}
